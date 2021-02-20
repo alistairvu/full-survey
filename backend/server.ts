b512a21fe863b5b2a04a6b19bb2652c227b5601c
Alistair Vu <alistairvu@gmail.com>
2021-02-20 23:17:03 +0700

    Prepare for deploy

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import connectDB from "./config/db"
 import morgan from "morgan"
 import dotenv from "dotenv"
+import path from "path"
 
 import userRoutes from "./routes/userRoutes"
 import questionRoutes from "./routes/questionRoutes"
@@ -16,8 +17,23 @@ const PORT = process.env.PORT || 6960
 
 app.use(express.json())
 app.use(cors())
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
+}
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(path.resolve(), "/frontend/build")))
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(path.resolve(), "frontend", "build", "index.html")
+    )
+  )
+} else {
+  app.get("/api", (req, res) => {
+    res.send("API is running...")
+  })
 }
 
 app.use("/api/users", userRoutes)
