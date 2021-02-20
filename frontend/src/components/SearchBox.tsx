e8d06b7ccbfcf08f08790e5b0578b74a3929b117
Alistair Vu <alistairvu@gmail.com>
2021-02-21 00:18:20 +0700

    Remove redundant import

--- frontend/src/components/SearchBox.tsx
+++ frontend/src/components/SearchBox.tsx
@@ -1,4 +1,4 @@
-import { Form, Button, Col } from "react-bootstrap"
+import { Form, Button } from "react-bootstrap"
 import { useHistory } from "react-router-dom"
 import { useState } from "react"
 
